13e2c63c65d1064df81d03895299b23264b55c67
T-Sajad <talalsajad300@gmail.com>
2023-09-27 17:39:38 +0100

    now displays a  filtered results of  games in the next seven days

--- src/components/Fixtures.jsx
+++ src/components/Fixtures.jsx
@@ -1,21 +1,21 @@
 import fixturedata from '../../data/get-fixtures-en.json'
 import {useState} from 'react'
 console.log(fixturedata)
+import { startOfDay, addDays, endOfDay } from 'date-fns'
 
 function Versus() {
   const fixture = fixturedata.response
-
-  const todaysdate = new Date ()
-  console.log(todaysdate)
-
-
   const [searchItem, setSearchItem] = useState('')
-  const [filteredUsers, setFilteredUsers] = useState(fixture)
-
+  //const [filteredUsers, setFilteredUsers] = useState(fixture)
+  const [filteredUsers, setFilteredUsers] = useState(filterSevenDays(fixture))
+  console.log(filteredUsers)
+  
+  // TODO give this a more meaningful name, like `filterByName`
   const handleInputChange = (e) => { 
-    const searchTerm = e.target.value;
-    setSearchItem(searchTerm)
+      const searchTerm = e.target.value;
+      setSearchItem(searchTerm)
 
+    // TODO this actual filter function is a great candidate for unit testing!
     const filteredItems = fixture.filter((fixtures) =>
     fixtures.teams.home.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
     fixtures.teams.away.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -24,21 +24,39 @@ function Versus() {
     setFilteredUsers(filteredItems);
   }
 
+  // TODO this is also a candidate for unit testing!
+  function filterSevenDays(fixtureData) {
+      const todaysdate = new Date ()
+      // https://date-fns.org/v2.30.0/docs/addDays
+      const startDate = startOfDay(todaysdate)
+      const endDate = endOfDay(addDays(todaysdate,6))
+      console.log(`filtering between ${startDate.toISOString()} and ${endDate.toISOString()}`)
+      return fixtureData.filter((fixtureData)=>{
+        const { fixture } = fixtureData;
+        const gameday = new Date(fixture.date)
+        if(gameday >= startDate && gameday <= endDate ) {
+          return fixture    
+        }
+    })
+  }
 
-
-  
   return (
     <div>
         <input 
         type="text"
         value={searchItem}
         onChange={handleInputChange}
-        placeholder='Type to search'
- />
+        placeholder='team'
+        />
       <div>
         {filteredUsers.map((fixture, i) => (
           <div key={i}>
-            <h3>{`${fixture.teams.home.name}`} <img src={fixture.teams.home.logo}/> {`${fixture.score.fulltime.home}-${fixture.score.fulltime.away}`} <img src={fixture.teams.away.logo}/> {`${fixture.teams.away.name}`}</h3>
+            <h3>{`${fixture.teams.home.name}`}
+              <img width='75pxpx' height='75px'src={fixture.teams.home.logo}/>
+              {`${fixture.score.fulltime.home}-${fixture.score.fulltime.away}`}
+              <img  width='75pxpx' height='75px' src={fixture.teams.away.logo}/>
+              {`${fixture.teams.away.name}`}
+            </h3>
           </div>
         ))}
       </div>
