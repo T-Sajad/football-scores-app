b4412a1746103f583705a8fe08d29def1c7070e4
T-Sajad <talalsajad300@gmail.com>
2023-10-17 16:42:00 +0100

    Refactor

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -4,18 +4,18 @@ function TabList({ renderPage, setCurrentPage }) {
   return (
     <div className="Tabs" style={{}}>
       <nav>
-        <ul>
-          <li>
+        <ul className="Tab">
+          <li className="TabList">
             <div onClick={() => setCurrentPage('Byteams')}>
               <h2>By Teams</h2>
             </div>
           </li>
-          <li>
+          <li className="TabList">
             <div onClick={() => setCurrentPage('Upcoming')}>
               <h2>Upcoming</h2>
             </div>
           </li>
-          <li>
+          <li className="TabList">
             <div onClick={() => setCurrentPage('Results')}>
               <h2>Results</h2>
             </div>
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -30,8 +30,8 @@ function Options() {
 
   return (
     <TabList
+      setCurrentPage={setCurrentPage}
       renderPage={renderPage}
-      setCurrentpage={setCurrentPage}
     />
   );
 }
