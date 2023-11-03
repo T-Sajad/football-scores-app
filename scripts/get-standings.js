ae3d2e0abf762111fe16968319da9a12d033a048
T-Sajad <talalsajad300@gmail.com>
2023-11-03 00:16:11 +0000

    updated fixtures

---
+++ scripts/get-standings.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import fs from 'node:fs';
+
+const url =
+  'https://api-football-v1.p.rapidapi.com/standings?season=2023&league=39&team=47';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.API_KEY,
+    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
+  },
+};
+
+try {
+  const response = await fetch(url, options);
+  const result = await response.text();
+  fs.writeFileSync('./data/get-standings.json', result);
+} catch (error) {
+  console.error(error);
+}
