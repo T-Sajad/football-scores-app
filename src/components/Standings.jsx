63a4473f75242887cd3a428c4cb082bd42f11392
T-Sajad <talalsajad300@gmail.com>
2023-11-09 16:53:27 +0000

    created table for the PL standings

--- src/components/Standings.jsx
+++ src/components/Standings.jsx
@@ -1,7 +1,15 @@
+import { tr } from 'date-fns/locale';
 import React from 'react';
 
 function Standings({ standing }) {
-  return <li>{`${standing.rank}. ${standing.team.name}`}</li>;
+  return (
+    <tr>
+      <td className="values">{`${standing.rank}`}</td>
+      <td className="values">{`${standing.team.name}`}</td>
+      <td className="values">{`${standing.points}`}</td>
+      <td className="values">{`${standing.goalsDiff}`}</td>
+    </tr>
+  );
 }
 
 export default Standings;
--- src/components/StandingsList.jsx
+++ src/components/StandingsList.jsx
@@ -6,13 +6,17 @@ function StandingsList() {
   const standings = standingData.response[0].league.standings[0]; // Note the correct property name here
 
   return (
-    <div>
-      <ul>
-        {standings.map((standing) => (
-          <Standings standing={standing} />
-        ))}
-      </ul>
-    </div>
+    <table>
+      <tr>
+        <th>Pos</th>
+        <th>club</th>
+        <th>PTS</th>
+        <th>GD</th>
+      </tr>
+      {standings.map((standing) => (
+        <Standings standing={standing} />
+      ))}
+    </table>
   );
 }
 
