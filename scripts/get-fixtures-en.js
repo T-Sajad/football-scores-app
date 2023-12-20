7302d2c6f7d50ef40ef882121526bb8aface8ebb
T-Sajad <talalsajad300@gmail.com>
2023-12-20 14:56:57 +0000

    created a helper fucntion help fecth data for all endpoints

--- scripts/get-fixtures-en.js
+++ scripts/get-fixtures-en.js
@@ -1,25 +1,7 @@
-import 'dotenv/config';
-import fs from 'node:fs';
+import helper from './fetchhelper.js';
 
 const url =
   'https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2023';
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': process.env.API_KEY,
-    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-  },
-};
+const name = 'get-fixtures-en';
 
-// console.log(process.env.API_KEY)
-
-try {
-  const response = await fetch(url, options);
-  const result = await response.json();
-  fs.writeFileSync(
-    './data/get-fixtures-en.json',
-    JSON.stringify(result, null, 2)
-  );
-} catch (error) {
-  console.error(error);
-}
+helper(url, name);
--- scripts/get-one-league.js
+++ scripts/get-one-league.js
@@ -1,24 +1,5 @@
-import 'dotenv/config';
-import fs from 'node:fs';
+import helper from './fetchhelper';
 
 const url = 'https://api-football-v1.p.rapidapi.com/v3/leagues';
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': process.env.API_KEY,
-    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-  },
-};
-
-// console.log(process.env.API_KEY)
-
-try {
-  const response = await fetch(url, options);
-  const result = await response.json();
-  fs.writeFileSync(
-    './data/one-league.json',
-    JSON.stringify(result.response[0], null, 2)
-  );
-} catch (error) {
-  console.error(error);
-}
+const name = 'get-one-league';
+helper(url, name);
